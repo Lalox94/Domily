6382921e1449ff3efcaddabef395df69c20688a9
Lalox94 <lalotemporal@gmail.com>
2017-10-16 13:46:50 -0500

    Agrega la comunicacion entre dos dispositivos

--- app/src/main/java/com/domily/android/domily/MainActivity.java
+++ app/src/main/java/com/domily/android/domily/MainActivity.java
@@ -4,6 +4,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.Toast;
 
 import com.firebase.ui.auth.AuthUI;
@@ -11,19 +12,24 @@ import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.messaging.FirebaseMessaging;
 
-import java.util.Arrays;
 
 public class MainActivity extends AppCompatActivity {
 
     private FirebaseAuth mFirebaseAuth;
     private FirebaseAuth.AuthStateListener mAuthStateListener;
-    private FirebaseDatabase mFirebaseDatabase;
-    private DatabaseReference mMessagesDatabaseReference;
-
     public static final int RC_SIGN_IN = 1;
+    private DatabaseReference mMessagesReference;
     public static final String ANONYMOUS = "anonymous";
+    private FirebaseDatabase mDatabase;
+
+
     private String mUsername;
+    private EditText mMessageEditText;
+    private Button btn;
+    private Button btn1;
+    private Button btn2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,27 +37,52 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         mUsername = ANONYMOUS;
+        mDatabase = FirebaseDatabase.getInstance();
+        mMessagesReference = mDatabase.getReference().child("messages");
 
-        Button btn = (Button) findViewById(R.id.ButtonSendMessage);
+         btn = (Button) findViewById(R.id.ButtonSendMessage);
+         btn1 = (Button) findViewById(R.id.ButtonSubscribe);
+         btn2 = (Button) findViewById(R.id.ButtonUnsubscribe);
+         mMessageEditText = (EditText) findViewById(R.id.EditTextSend);
+
+
 
         //  Envia un valor a la Base de datos
         btn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 // TODO: Send messages on click
-                mMessagesDatabaseReference.push().setValue("Sup my nigga");
+                Message message = new Message(mMessageEditText.getText().toString(), mUsername, null);
+                mMessagesReference.push().setValue(message);
+                mMessageEditText.setText("");
 
             }
         });
 
+        //  Envia un valor a la Base de datos
+        btn1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // TODO: Send messages on click
+                FirebaseMessaging.getInstance().subscribeToTopic("pushNotifications");
+                Toast.makeText(MainActivity.this, "Subscribed to Topic: Push Notifications", Toast.LENGTH_SHORT).show();
+            }
+        });
+        //  Envia un valor a la Base de datos
+        btn2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // TODO: Send messages on click
+                FirebaseMessaging.getInstance().unsubscribeFromTopic("pushNotifications");
+                Toast.makeText(MainActivity.this, "Unsubscribed to Topic: Push Notifications", Toast.LENGTH_SHORT).show();
+
+            }
+        });
+
+
+
         // Access Point
-        mFirebaseDatabase = FirebaseDatabase.getInstance();
-        //mFirebaseAuth = FirebaseAuth.getInstance();
-        mMessagesDatabaseReference = mFirebaseDatabase.getReference().child("messages");
-
-
-
-        /*
+        mFirebaseAuth = FirebaseAuth.getInstance();
 
         // Login
         mAuthStateListener = new FirebaseAuth.AuthStateListener() {
@@ -62,6 +93,7 @@ public class MainActivity extends AppCompatActivity {
                 if(user != null) {
                     // User is sign in
                     onSignedInInitialize(user.getDisplayName());
+
                 } else {
                     // User is signed out
                     onSignedOutCleanup();
@@ -78,7 +110,7 @@ public class MainActivity extends AppCompatActivity {
                 }
             }
         };
-        */
+
 
     }
 
@@ -93,12 +125,13 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onResume() {
         super.onResume();
-      //  mFirebaseAuth.addAuthStateListener(mAuthStateListener);
+        mFirebaseAuth.addAuthStateListener(mAuthStateListener);
     }
 
     @Override
     protected void onPause() {
         super.onPause();
-       // mFirebaseAuth.addAuthStateListener(mAuthStateListener);
+        mFirebaseAuth.addAuthStateListener(mAuthStateListener);
     }
+
 }

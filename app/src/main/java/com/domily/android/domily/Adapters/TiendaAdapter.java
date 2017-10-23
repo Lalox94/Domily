e225884d2c5f34242fe1df8a507be9664e9760e9
Lalox94 <lalotemporal@gmail.com>
2017-10-22 22:00:29 -0500

    Agregar Packetes para Activities, Fragments & Adapters

--- app/src/main/java/com/domily/android/domily/Adapters/TiendaAdapter.java
+++ app/src/main/java/com/domily/android/domily/Adapters/TiendaAdapter.java
@@ -1,10 +1,12 @@
-package com.domily.android.domily;
+package com.domily.android.domily.Adapters;
 
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
+
+import com.domily.android.domily.R;
 
 import java.util.List;
 
